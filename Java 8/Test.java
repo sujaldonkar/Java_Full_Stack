dcf1dc36231e35e98aff0461bab02e340a0ba40a
sujaldonkar <sujaldonkar7@gmail.com>
2026-08-03 18:35:35 +0530

    Add Test

---
+++ Java 8/Test.java
@@ -0,0 +1,30 @@
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Optional<String> name=getName(2);
+//		if(name.isPresent()) {
+//			System.out.println(name.get());
+//		}
+//		name.ifPresent(x->System.out.println(x));
+		
+//		String nameToUsed=name.orElseGet(()->"NA");
+		
+//		String nameToUsed=name.orElseThrow(()->new NoSuchElementException());
+//		System.out.println(nameToUsed);
+		
+		name.map(String::toUpperCase).ifPresent(System.out::println);
+	}
+	private static Optional<String> getName(int id) {
+		String name="Ram";
+		return Optional.of(name);
+		
+//		String name=null;
+//		return Optional.ofNullable(name);
+		
+//		return Optional.empty();
+	}
+
+}
