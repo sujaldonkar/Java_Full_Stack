dfea8c8f0b99020d90cbd13cc254812b0f806ea1
sujaldonkar <sujaldonkar7@gmail.com>
2026-08-03 18:31:59 +0530

    Add prgm

---
+++ Java 8/GreetingDemo.java
@@ -0,0 +1,13 @@
+public class GreetingDemo {
+
+	public static void main(String[] args) {
+		Greeting g1=()->{
+			System.out.println("Hello Sujal");
+		};// Lambda Expression
+		
+		g1.greet();
+
+
+	}
+
+}
